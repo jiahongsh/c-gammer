d01687bd29e786885aefb1c2c8b0f5b2eb03a363
a-jia_1_0 <3378079760@qq.com>
2024-03-09 01:01:47 +0800

    递归与递推

---
+++ 递推/递推/test.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+//找规律递推算法
+int main() {
+	
+	int level = 1;
+	int sum = 1;
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		level += i;
+		sum += level;
+	}
+	cout << sum << endl;
+	return 0;
+}
---
+++ 递推与递归的比较/递推与递归的比较/test.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+//递归方式实现，时间复杂度大，重复计算
+//long long fib(int n) 
+//{
+//	if (n == 1 || n == 2) {
+//		return 1;
+//	}
+//	return fib(n - 1) + fib(n - 2);
+//}
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//		cout << fib(n) << endl;
+//	
+//	
+//	return 0;
+//}
+
+
+//时间复杂度低，程序运行时间短
+int main() {
+	long long arr[60] = { 0 };
+	arr[1] = arr[2] = 1;
+	 
+	for (int i = 3; i < 50; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	
+	for (int i = 1; i <= 50;i++) {
+		cout << i << ":" << arr[i] << endl;
+	}
+	return 0;
+}
