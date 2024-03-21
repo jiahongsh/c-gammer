fb86e2cfc59fe6aa5cbdc27d34e6f6f1d0b1bf7b
a-jia_1_0 <3378079760@qq.com>
2024-03-21 21:05:30 +0800

    位运算，快速幂

---
+++ 快速幂/快速幂/test.cpp
@@ -0,0 +1,23 @@
+﻿//快速幂，计算2020的100000此方取后四位
+#include<iostream>
+using namespace std;
+int mod = 10000;
+
+int main() {
+	long long a, b;
+	cin >>a >> b;
+	long long ans = 1;
+ 	while (b) {
+		if (b & 1) {
+			ans *= a;
+			ans = ans % mod;
+
+		}
+		b >>= 1;
+		a *= a;
+		a = a % mod;
+
+	}
+	cout<< ans;
+}
+
