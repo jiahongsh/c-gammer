6e52ee86dc7a83ece4b0eb380a26acf7bfda8cb5
a-jia_1_0 <3378079760@qq.com>
2024-03-22 20:33:49 +0800

    时间复杂度的降低

---
+++ 递归算法的优化/递归算法的优化/test.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int fib(int n,int*data) {
+
+	
+	if (n == 1 || n == 2) {
+		data[n] = 1;
+		return data[n];
+	}
+
+	if (data[n] != 0)
+	{
+		return data[n];
+	}
+	data[n] = fib(n - 1,data) + fib(n - 2,data);//将已经计算过的数据保存起来，避免重复计算
+	return data[n];
+	
+}
+int main() {
+	int data[50] = {0};
+	int num;
+	cin >> num;
+	cout << fib(num,data);
+	return 0;
+}
+
